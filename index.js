4f604754f52bbf8fedcf8bd4a10b3608ea61ed97
Vlatko Juric <juricv78gmail.com>
2023-10-10 13:57:57 +0200

    half done

--- index.js
+++ index.js
@@ -21,15 +21,19 @@ const animalStrings = [
   "rhino",
 ];
 
-const hippoExists = null;
+const hippoExists = animalStrings.includes("hippo");
 
 // Hint: You can pass the starting index as second parameter.
-const catStartingFromIndexFiveExists = null;
+const catStartingFromIndexFiveExists = animalStrings.includes("cat", 5);
 
 // Hint: Besides the array method, check out the string method `startsWith()`.
-const firstAnimalStartingWithLetterP = null;
+const firstAnimalStartingWithLetterP = animalStrings.find((animal) =>
+  animal.startsWith("p")
+);
 
-const indexOfGiraffe = null;
+const indexOfGiraffe = animalStrings.findIndex((animal) =>
+  animal.startsWith("g")
+);
 
 // Note:
 // - Sorting strings is slightly more complicated than sorting numbers.
